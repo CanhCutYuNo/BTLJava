69d6b51b91c3df033b53622d722f077b470a83ff
hungtvlt <tuanhungvu0912@gmail.com>
2025-02-23 18:07:25 +0700

    Improve performance

--- src/gui/jPanel/BackgroundJPanel.java
+++ src/gui/jPanel/BackgroundJPanel.java
@@ -7,12 +7,17 @@ package gui.jPanel;
 import application.GameSettings;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 
 /**
@@ -22,94 +27,29 @@ import javax.swing.JComponent;
 public class BackgroundJPanel extends javax.swing.JPanel {
 
     private final GameSettings gameSettings;
-    private final Thread _updateThread;
-    private final Thread _drawThread;
-    private final List<Star> synchronizedStars;
+    private final Thread drawThread;
+    private Image backgroundImage1;
+    private Image backgroundImage2;
+    private int y1;
+    private int y2;
     private boolean isRunning = true;
-
-    private class Star {
-
-        public Star(int x, int y, int width, int height) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-        }
-        
-        public Star(int x, int y, int edge) {
-            this.x = x;
-            this.y = y;
-            this.width = edge;
-            this.height = edge;
-        }
-
-        private final int width;
-        private final int height;
-        private int x;
-        private int y;
-    }
 
     public BackgroundJPanel(GameSettings gameSettings) {
         this.gameSettings = gameSettings;
-        _drawThread = new Thread(() -> {
+        backgroundImage1 = new ImageIcon(getClass().getResource("/asset/resources/backgrounds/1.png")).getImage();
+        backgroundImage2 = new ImageIcon(getClass().getResource("/asset/resources/backgrounds/1.png")).getImage();
+//        backgroundImage1 = backgroundImage1.getScaledInstance(gameSettings.getHeight(), gameSettings.getWidth(), Image.SCALE_AREA_AVERAGING);
+//        backgroundImage2 = backgroundImage2.getScaledInstance(gameSettings.getHeight(), gameSettings.getWidth(), Image.SCALE_AREA_AVERAGING);   
+        y1 = 0;
+        y2 = gameSettings.getHeight();
+        drawThread = new Thread(() -> {
             drawLoop();
         });
-        _updateThread = new Thread(() -> {
-            updateLoop();
-        });
-        synchronizedStars = Collections.synchronizedList(new LinkedList<Star>());
-        _drawThread.start();
-        _updateThread.start();
+        drawThread.start();
     }
 
-    public void Stop() {
+    public void stop() {
         isRunning = false;
-    }
-
-    private void loop() {
-        long currentTime;
-        long invokeTime;
-        invokeTime = currentTime = System.nanoTime();
-        while (isRunning) {
-            while (currentTime - invokeTime < 1000000000 / gameSettings.getFPS()) {
-                currentTime = System.nanoTime();
-            }
-            update();
-            repaint();
-            invokeTime = currentTime;
-        }
-    }
-
-    private void updateLoop() {
-        long currentTime;
-        long invokeTime;
-        invokeTime = currentTime = System.nanoTime();
-        while (isRunning) {
-            while (currentTime - invokeTime < 1000000000 / gameSettings.getTPS()) {
-                currentTime = System.nanoTime();
-            }
-//            System.out.println("RUNNING");
-            update();
-            invokeTime = currentTime;
-        }
-    }
-
-    private void update() {
-        Random random = new Random();
-        synchronized (synchronizedStars) {
-            synchronizedStars.add(new Star(random.nextInt() % gameSettings.getWidth(), gameSettings.getHeight() + 1, random.nextInt() % 2 + 2));
-            //Complexity: O(n2)
-            for (Iterator it = synchronizedStars.iterator(); it.hasNext();) {
-                Star star = (Star) it.next();
-                if (star.y < 0) {
-                    synchronizedStars.remove(star);
-                    it = synchronizedStars.iterator();
-                    continue;
-                }
-                star.y = star.y - 1;
-            }
-        }
-
     }
 
     private void drawLoop() {
@@ -118,28 +58,33 @@ public class BackgroundJPanel extends javax.swing.JPanel {
         invokeTime = currentTime = System.nanoTime();
         while (isRunning) {
             while (currentTime - invokeTime < 1000000000 / gameSettings.getFPS()) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
                 currentTime = System.nanoTime();
             }
+            update();
             repaint();
             invokeTime = currentTime;
         }
     }
 
+    private void update() {
+        if (y1 == -gameSettings.getHeight()) {
+            y1 = gameSettings.getHeight();
+        }
+        if (y2 == -gameSettings.getHeight()) {
+            y2 = gameSettings.getHeight();
+        }
+        y1--;
+        y2--;
+    }
+
     @Override
     public void paint(Graphics g) {
-        g.clearRect(0, 0, gameSettings.getWidth(), gameSettings.getHeight());
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, gameSettings.getWidth(), gameSettings.getHeight());
-        synchronized (synchronizedStars) {
-            for (Object object : synchronizedStars) {
-                if (object == null) {
-                    continue;
-                }
-                Star star = (Star) object;
-                g.setColor(Color.WHITE);
-                g.fillOval(star.x, star.y, star.width, star.height);
-            }
-        }
-
+        g.drawImage(backgroundImage1, 0, y1, gameSettings.getWidth(), gameSettings.getHeight(), null);
+        g.drawImage(backgroundImage2, 0, y2, gameSettings.getWidth(), gameSettings.getHeight(), null);
     }
 }
